0b5dfafc8b463be980b15d8093f9e6afc5359571
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-06-04 14:15:14 +0530

    text area

--- components/notes/index.jsx
+++ components/notes/index.jsx
@@ -4,7 +4,7 @@ const index = () => {
   return (
     <div className='bg-[#212529]/75 rounded-lg flex flex-col h-[30vh] p-4'>
       <p className='text-2xl text-center mb-4'>Notes</p>
-      <textarea className='bg-[#212529] p-4 overflow-y-scroll rounded-lg focus:outline-none  w-full flex-grow'/>
+      <textarea className='bg-[#212529] p-4 overflow-y-scroll rounded-lg focus:outline-none text-white/50  w-full flex-grow'/>
     </div>
   )
 }
