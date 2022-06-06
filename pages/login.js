31634d6a523d12fe37d1ee8c4c21d92255067d38
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-06-06 08:28:46 +0530

    lastest draft

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React,{useState,useEffect,useContext} from 'react'
 import { POST } from '../apihelper'
@@ -6,11 +7,18 @@ import { AppContext } from '../AppContext'
 const Login = () => {
   const router = useRouter()
   const [data, setdata] = useState({username:"",password:""})
+  const [error,setError]=useState("")
   const {setjwt,setuser,state}=useContext(AppContext)
   const handle=async(e)=>{
     e.preventDefault()
-    const res= await POST(data,"auth/login")
+    const res={}
+    try{
+      res= await POST(data,"auth/login")
+    }catch(err){
+      setError("Invalid username or password")
+    }
     if(res.statusText==="OK"){
+      setError("")
       setuser(res.data.user)
       setjwt(res.data.accessToken)
       router.push("/")
@@ -28,11 +36,15 @@ const Login = () => {
         <img draggable={false}  src='/svg/logo.svg'/> 
         <p className='mt-8 mb-8 text-5xl text-white font-extralight'>Login</p>
         <form onSubmit={handle} className='flex flex-col items-center'> 
-          <p className='w-full text-xl text-white/50 mt-4 mb-1'>Username</p>
+          <p className='h-4 mt-2 text-red-500/75'>{error}</p>
+          <p className='w-full text-xl text-white/50 mt-2 mb-1'>Username</p>
           <input value={data.username} onChange={e=>setdata({...data,username:e.target.value})}  type='text' className='w-[300px] border-2 border-white/50 rounded-lg px-4 py-2 bg-transparent text-white/50 focus:outline-none'/>
           <p className='w-full text-xl text-white/50 mt-4 mb-1'>Password</p>
           <input value={data.password} onChange={e=>setdata({...data,password:e.target.value})}  type='password' className='w-[300px] border-2 border-white/50 rounded-lg px-4 py-2 bg-transparent text-white/50 focus:outline-none'/>  
-          <button className='w-56 py-3 px-4 rounded-xl text-2xl mt-12 border-2 border-white/75 text-white'>LOGIN</button>
+          <div className='flex flex-col items-center'>
+            <button className='w-56 py-3 px-4 rounded-xl text-2xl mt-12 border-2 border-white/75 text-white'>LOGIN</button>
+            <Link href='/signup'><p className='hover:mb-0 mb-[2px] text-md mt-3  text-white/75 hover:border-b-2 hover:border-white/50'>Don't have an Account, Create one</p></Link>
+          </div>
         </form> 
       </div>
     </div>
--- pages/signup.js
+++ pages/signup.js
@@ -1,19 +1,24 @@
 import React,{useState,useEffect,useContext} from 'react'
 import { useRouter } from 'next/router'
 import { AppContext } from '../AppContext'
+import Link from 'next/link'
+
 
 const Login = () => {
   const router = useRouter()
   const {setjwt,setuser,state}=useContext(AppContext)
+  const [error,setError]=useState("")
   const [data, setdata] = useState({name:"",username:"",password:""})
   const handle=async(e)=>{
     e.preventDefault()
-    const res= await fetch("https://pomo-hackfest.herokuapp.com/api/auth/register",{method:"post",body:JSON.stringify(data),headers:{'Content-Type':'application/json'}})
-    if (res.ok){
+    const res= await(await fetch("https://pomo-hackfest.herokuapp.com/api/auth/register",{method:"post",body:JSON.stringify(data),headers:{'Content-Type':'application/json'}})).json()
+    if (res.accessToken){
       console.log("ok")
-      setuser(res.data.user)
-      setjwt(res.data.accessToken)
+      setuser(res.user)
+      setjwt(res.accessToken)
       router.push("/")
+    }else{
+      setError(res.message)
     }
   }
   useEffect(()=>{
@@ -28,14 +33,17 @@ const Login = () => {
         <img draggable={false}  src='/svg/logo.svg'/> 
         <p className='mt-8 mb-8 text-5xl text-white font-extralight'>Sign Up</p>
         <form onSubmit={handle} className='flex flex-col items-center'> 
-          <p className='w-full text-xl text-white/50 mt-4 mb-1'>Name</p>
+        <p className='h-4 mt-2 text-red-500/75'>{error}</p>
+          <p className='w-full text-xl text-white/50 mt-2 mb-1'>Name</p>
           <input value={data.name} onChange={e=>setdata({...data,name:e.target.value})}  type='text' className='w-[300px] border-2 border-white/50 rounded-lg px-4 py-2 bg-transparent text-white/50 focus:outline-none'/>
           <p className='w-full text-xl text-white/50 mt-4 mb-1'>Username</p>
           <input value={data.username} onChange={e=>setdata({...data,username:e.target.value})}  type='text' className='w-[300px] border-2 border-white/50 rounded-lg px-4 py-2 bg-transparent text-white/50 focus:outline-none'/>
           <p className='w-full text-xl text-white/50 mt-4 mb-1'>Password</p>
           <input value={data.password} onChange={e=>setdata({...data,password:e.target.value})}  type='password' className='w-[300px] border-2 border-white/50 rounded-lg px-4 py-2 bg-transparent text-white/50 focus:outline-none'/>  
-          <button className='w-56 py-3 px-4 rounded-xl text-2xl mt-12 border-2 border-white/75 text-white'>SignUp</button>
-        </form> 
+          <div className='flex flex-col items-center'>
+            <button className='w-56 py-3 px-4 rounded-xl text-2xl mt-12 border-2 border-white/75 text-white'>SIGN UP</button>
+            <Link href='/login'><p className='hover:mb-0 mb-[2px] text-md mt-3  text-white/75 hover:border-b-2 hover:border-white/50'>Already have an account? Login</p></Link>
+          </div>        </form> 
       </div>
     </div>
   )
